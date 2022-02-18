40267df08151b56035ef5bb8bda6d638dda7d05f
Monika <monikasingh.singh95@gmail.com>
2022-02-18 22:38:57 +0530

    Using variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  let x=5;
+  let y=20;
+  let valueData = xyz();
+  let config = getConfig(valueData);
   return (
+
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -10,6 +17,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hey {x} {addTwoNo(x, y)}</p>
+        <div>
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <input value="Submit" onClick={handler}/>
+          </form>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +37,26 @@ function App() {
         </a>
       </header>
     </div>
+
+
   );
 }
 
+const addTwoNo = (a, b) => {
+  return a+b;
+}
+
+const handler = () =>  console.log("hello");
+
+const xyz = () => {
+  let valueData = null;
+  return valueData;
+}
+
+const getConfig = (valueData) => {
+  let x= null;
+  return x;
+}
+
 export default App;
+
